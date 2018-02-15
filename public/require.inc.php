f3940aca3b6679d5b5d3af8718cd9662c6c73806
Satoru Kikuchi <satoruk@smocca.co.jp>
2018-02-15 17:35:22 +0900

    Fix the names of constant (PATH_PUBLIC, PATH_PRIVATE to PUBLIC_PATH, PRIVATE_PATH)

--- public/require.inc.php
+++ public/require.inc.php
@@ -1,5 +1,5 @@
 <?php
 // Require
 require_once(dirname(__FILE__) . "/../private/config.inc.php");
-require_once(PATH_PRIVATE . "/library/kyweb.inc.php");
+require_once(PRIVATE_PATH . "/library/kyweb.inc.php");
 ?>
